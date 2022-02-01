0e9e96f7dd3a7394e0588dd07126d15642cfe997
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-01 19:59:18 +0530

    Omk

--- TGNRobot/__init__.py
+++ TGNRobot/__init__.py
@@ -223,7 +223,7 @@ dispatcher = updater.dispatcher
 bot = Client('bot',
              api_id=os.environ.get('API_ID'),
              api_hash=os.environ['API_HASH'],
-             bot_token=os.environ['BOT_TOKEN'],
+             bot_token=os.environ['TOKEN'],
 )
 
 musicbot = Client(
